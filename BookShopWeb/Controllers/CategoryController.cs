c7f2c0e07f88ede1be997121efa40ebd97c9205e
Sushen99 <sushen.jnv.99@gmail.com>
2023-01-13 21:12:57 +0530

    [Category]:-Display All Category

--- BookShopWeb/Controllers/CategoryController.cs
+++ BookShopWeb/Controllers/CategoryController.cs
@@ -1,15 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BookShopWeb.Data;
+using BookShopWeb.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BookShopWeb.Controllers
 {
     public class CategoryController : Controller
     {
-        public class Category : Controller
-        { 
-            public IActionResult Index()
-            {
-                return View();
-            }
+        private readonly ApplicationDbContext _context;
+
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryLIst = _context.Categories.ToList();
+            return View(objCategoryLIst);
         }
     }
 }
