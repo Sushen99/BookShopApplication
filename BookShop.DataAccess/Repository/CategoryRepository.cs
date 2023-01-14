5c7c5e4235a98ed1b84256cfe86d36ffbd5f76a2
Sushen99 <sushen.jnv.99@gmail.com>
2023-01-14 12:51:27 +0530

    [CategoryRepository]:-implemented Category Repo.

---
+++ BookShop.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,31 @@
+﻿using BookShop.DataAccess.Repository.IRepository;
+using BookShopWeb.DataAccess;
+using BookShopWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookShop.DataAccess.Repository
+{
+	public class CategoryRepository : Repository<Category>, ICategoryRepository
+	{
+		private ApplicationDbContext _context;
+
+		public CategoryRepository(ApplicationDbContext context) : base(context)
+		{
+			_context = context;
+		}
+
+		void ICategoryRepository.Save()
+		{
+			_context.SaveChanges();
+		}
+
+		void ICategoryRepository.Update(Category obj)
+		{
+			_context.Categories.Update(obj);
+		}
+	}
+}
---
+++ BookShop.DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,16 @@
+﻿using BookShopWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookShop.DataAccess.Repository.IRepository
+{
+    public interface ICategoryRepository : IRepository<Category>
+    {
+        void Update(Category obj);
+
+        void Save();
+    }
+}
