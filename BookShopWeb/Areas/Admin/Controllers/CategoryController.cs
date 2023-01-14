4396d87f7038dedda9cb956569335440aff72d05
Sushen99 <sushen.jnv.99@gmail.com>
2023-01-14 18:01:24 +0530

    [Area]:-Area in Action for category type

--- BookShopWeb/Areas/Admin/Controllers/CategoryController.cs
+++ BookShopWeb/Areas/Admin/Controllers/CategoryController.cs
@@ -3,15 +3,16 @@ using BookShopWeb.DataAccess;
 using BookShopWeb.Models;
 using Microsoft.AspNetCore.Mvc;
 
-namespace BookShopWeb.Controllers
+namespace BookShopWeb.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class CategoryController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
 
         public CategoryController(IUnitOfWork unitOfWork)
         {
-			_unitOfWork = unitOfWork;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
@@ -40,8 +41,8 @@ namespace BookShopWeb.Controllers
 
             if (ModelState.IsValid)
             {
-				_unitOfWork.Category.Add(obj);
-				_unitOfWork.Save();
+                _unitOfWork.Category.Add(obj);
+                _unitOfWork.Save();
                 TempData["success"] = "Category created successfully";// tempdata
                 return RedirectToAction("Index");
             }
@@ -49,43 +50,43 @@ namespace BookShopWeb.Controllers
         }
 
 
-		//GET
-		public IActionResult Edit(int? id)
-		{
-			if (id == null || id == 0)
-			{
-				return NotFound();
-			}
-			//var categoryFromDb = _context.Categories.Find(id);
-			var categoryFromDbFirst = _unitOfWork.Category.GetFirstOrDefault(u => u.Id == id);
-			//var categoryFromDbSingle = _context.Categories.SingleOrDefault(u => u.Id == id);
+        //GET
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            //var categoryFromDb = _context.Categories.Find(id);
+            var categoryFromDbFirst = _unitOfWork.Category.GetFirstOrDefault(u => u.Id == id);
+            //var categoryFromDbSingle = _context.Categories.SingleOrDefault(u => u.Id == id);
 
-			if (categoryFromDbFirst == null)
-			{
-				return NotFound();
-			}
+            if (categoryFromDbFirst == null)
+            {
+                return NotFound();
+            }
 
-			return View(categoryFromDbFirst);
-		}
+            return View(categoryFromDbFirst);
+        }
 
-		//POST
-		[HttpPost]
-		[ValidateAntiForgeryToken]
-		public IActionResult Edit(Category obj)
-		{
-			if (obj.Name == obj.DisplayOrder.ToString())
-			{
-				ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
-			}
-			if (ModelState.IsValid)
-			{
-				_unitOfWork.Category.Update(obj);
-				_unitOfWork.Save();
-				TempData["success"] = "Category updated successfully";
-				return RedirectToAction("Index");
-			}
-			return View(obj);
-		}
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
+            }
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Category.Update(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "Category updated successfully";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
         public IActionResult Delete(int? id)
         {
             if (id == null || id == 0)
@@ -111,14 +112,14 @@ namespace BookShopWeb.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult DeletePOST(int? id)
         {
-            var obj = _unitOfWork.Category.GetFirstOrDefault(u=>u.Id ==id);
+            var obj = _unitOfWork.Category.GetFirstOrDefault(u => u.Id == id);
             if (obj == null)
             {
                 return NotFound();
             }
 
-			_unitOfWork.Category.Remove(obj);
-			_unitOfWork.Save();
+            _unitOfWork.Category.Remove(obj);
+            _unitOfWork.Save();
             TempData["success"] = "Category deleted successfully";
             return RedirectToAction("Index");
 
