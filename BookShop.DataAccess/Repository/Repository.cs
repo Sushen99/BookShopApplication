a98239f31c0836b52fe52e7649494d7a615fbab4
Sushen99 <sushen.jnv.99@gmail.com>
2023-01-16 21:37:47 +0530

    [Product]-include properties (category ) in product view

--- BookShop.DataAccess/Repository/Repository.cs
+++ BookShop.DataAccess/Repository/Repository.cs
@@ -17,21 +17,38 @@ namespace BookShop.DataAccess.Repository
 		public Repository(ApplicationDbContext context)
 		{
 			_context = context;
+			// _context.Products.Include(u=> u.Category).Include(u=>u.CoverType);
 			this.dbSet = context.Set<T>();
 		}
 		public void Add(T entity)
 		{
 			dbSet.Add(entity); 
 		}
-		public IEnumerable<T> GetAll()
+
+		// includeProp = "Category,CoverType"
+		public IEnumerable<T>GetAll(string? includeProperties = null)
 		{
 			IQueryable<T> query = dbSet;
+			if(includeProperties!= null)
+			{
+				foreach(var includeProp in includeProperties.Split(new char[] {','}, StringSplitOptions.RemoveEmptyEntries))
+				{
+					query = query.Include(includeProp);
+				}
+			}
 			return query.ToList();
 		}
-		public T GetFirstOrDefault(Expression<Func<T,bool>> filter)
+		public T GetFirstOrDefault(Expression<Func<T,bool>> filter,string? includeProperties = null)
 		{
 			IQueryable<T> query = dbSet;
 			query = query.Where(filter);
+			if(includeProperties != null)
+			{
+				foreach(var includeProp in includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+				{
+                    query = query.Include(includeProp);
+                }
+            }
 			return query.FirstOrDefault();
 		}
 
